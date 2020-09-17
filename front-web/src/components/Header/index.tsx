203503be163969fea9e8e83df26767e231dd9ea8
Denis-san <denis.san.nc@gmail.com>
2020-09-16 22:01:45 -0300

    Criada a navegacao por rotas

--- front-web/src/components/Header/index.tsx
+++ front-web/src/components/Header/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/logo.svg'
 import './styles.css';
 
 const Header = () => (
     <header className="main-header">
-      <Logo />
-      <div className="logo-text">
-            <span className="logo-text-1">Big Game</span>
-            <span className="logo-text-2"> Survey</span>
+      <Link to="/">
+           <Logo />
+      </Link>
+      
+      <Link to="/">
 
-      </div>
+        <div className="logo-text">
+              <span className="logo-text-1">Big Game</span>
+              <span className="logo-text-2"> Survey</span>
 
+        </div>
+      </Link>
     </header>
 );
 
