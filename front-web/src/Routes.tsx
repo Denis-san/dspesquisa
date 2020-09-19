27fb22ad5bf6912150132bdc4f1495074b89f187
Denis-san <denis.san.nc@gmail.com>
2020-09-18 21:39:25 -0300

    Adicionado a pagina de graficos

--- front-web/src/Routes.tsx
+++ front-web/src/Routes.tsx
@@ -1,6 +1,7 @@
 import React from  'react';
 import { BrowserRouter, Switch,  Route } from 'react-router-dom';
-import Header from './components/Header';
+import Header from './Components/Header';
+import Charts from './pages/Charts';
 import Home from './pages/Home';
 import Records from './pages/Records';
 
@@ -17,6 +18,11 @@ const Routes = () => (
             <Route path="/records">
                 <Records />
             </Route>
+
+            <Route path="/charts">
+                <Charts />
+            </Route>
+
         </Switch>
 
     </BrowserRouter>
